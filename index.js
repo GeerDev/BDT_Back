c61264556daaa44c4f13acbb618baa88b9a7942b
Ger <gerfersie@gmail.com>
2022-11-02 14:36:51 +0100

    Model company created

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const { dbConnection } = require('./database/config')
-
 const PORT = process.env.PORT
 
 dbConnection()
