6e881ff1d497505f5694b399893dcccfb9d07d69
Ger <gerfersie@gmail.com>
2022-11-08 15:39:12 +0100

    Swagger documentation finish

--- docs/company.js
+++ docs/company.js
@@ -3,7 +3,7 @@ module.exports = {
       "/companies/fillDatabase": {
         get: {
           tags: {
-            Functionality: " : Creation Company Database",
+            Endpoint: " : Creation Company Database",
           },
           description: "Create the database with the data obtained from the JSON of all the companies",
           operationId: "fillDatabase",
@@ -18,13 +18,73 @@ module.exports = {
         }
       },   
       "/companies/sortBySize":{
-        
+        get: {
+            tags: {
+              Endpoint: " : Companies sorted by size",
+            },
+            description: "Companies are displayed sorted by size in ascending order",
+            operationId: "sortBySize",
+            responses: {
+              200: {
+                content: {
+                    "application/json": {
+                      schema: {
+                        $ref: "#/components/schemas/companiesSchema",
+                      }
+                    }
+                  }
+              },
+              500: {
+                  description: "There was a problem sorting the companies by size",
+              }
+            }
+          }
       },   
       "/companies/sortByDate":{
-
+        get: {
+            tags: {
+              Endpoint: " : Companies sorted by date",
+            },
+            description: "Companies are displayed sorted by date of foundation",
+            operationId: "sortByDate",
+            responses: {
+              200: {
+                content: {
+                    "application/json": {
+                      schema: {
+                        $ref: "#/components/schemas/companiesSchema",
+                      }
+                    }
+                  }
+              },
+              500: {
+                  description: "There was a problem sorting the companies by date",
+              }
+            }
+          }
       },
       "/companies/numberOfCompanies":{
-
+        get: {
+            tags: {
+              Endpoint: " : Get companies by type",
+            },
+            description: "Shows the number of companies in each industry, for each size and for each date of creation",
+            operationId: "numberOfCompanies",
+            responses: {
+              200: {
+                content: {
+                    "application/json": {
+                      schema: {
+                        $ref: "#/components/schemas/numberOfCompaniesSchema",
+                      }
+                    }
+                  }
+              },
+              500: {
+                  description: "There was a problem finding the companies by type",
+              }
+            }
+          }
       }
     }
   }
--- docs/components.js
+++ docs/components.js
@@ -1,65 +1,103 @@
 module.exports = {
     components:{
         schemas:{
-            companySchema:{
+            companiesSchema:{
+                type:'array',
+                items:{
+                    type:'object',
+                    properties:{
+                        _id:{
+                            type:'ObjectId',
+                            description:"Unique identifier of the company stored in the database",
+                            example:"6368371c5a4d70807ae8ddc6"
+                        },
+                        id:{
+                            type:'String',
+                            description:"Unique identifier of the company",
+                            example:"akgbudapest"
+                        },
+                        website:{
+                            type:"String",
+                            description:"Company website",
+                            example:"akg.hu"
+                        },
+                        name:{
+                            type:'String',
+                            description:"Name of the company",
+                            example:"alternatív közgazdasági gimnázium"
+                        },
+                        founded:{
+                            type:'Number',
+                            description:"Year the company was created",
+                            example: 1988
+                        },
+                        size:{
+                            type:"String",
+                            description:"Company size",
+                            example: "51-200"
+                        },
+                        locality:{
+                            type:"String",
+                            description:"Company location",
+                            example:"budapest"
+                        },
+                        region:{
+                            type:"String",
+                            description:"Company region",
+                            example:"budapest"
+                        },
+                        country:{
+                            type:"String",
+                            description:"Country of company",
+                            example:"hungary"
+                        },
+                        industry:{
+                            type:"String",
+                            description:"Company type",
+                            example:"education management"
+                        },
+                        linkedin_url:{
+                            type:"String",
+                            description:"Company linkedin link",
+                            example:"linkedin.com/company/akgbudapest"
+                        },
+                        createdAt:{
+                            type:"String",
+                            description:"Creation date",
+                            example:"2022-11-06T22:37:16.571Z"
+                        },
+                        updatedAt:{
+                            type:"String",
+                            description:"Update date",
+                            example:"2022-11-06T22:37:16.571Z"
+                        },
+                        __v:{
+                            type:"String",
+                            description:"Set property of the document when it is first created",
+                            example: 0
+                        }
+                    }
+                }           
+            },
+            numberOfCompaniesSchema:{
                 type:'object',
                 properties:{
-                    _id:{
-                        type:'ObjectId',
-                        description:"Número identificación del post",
-                        example:"6201064b0028de7866e2b2c4"
-                    },
-                    id:{
-                        type:'String',
-                        description:"Título del post",
-                        example:"Titulo post"
-                    },
-                    website:{
-                        type:"String",
-                        description:"Descripción del post",
-                        example:"Descripción post"
-                    },
-                    name:{
-                        type:'String',
-                        description:"Imagen del post",
-                        example:"Path de la imagen del post"
-                    },
-                    founded:{
-                        type:'Number',
-                        description:"Usuario que ha hecho el post",
-                        example:"6201064b0028de7866e2b2c4"
-                    },
-                    size:{
-                        type:"String",
-                        description:"Comentarios dentro del post, es un array de objetos",
-                        example:"userId, comment, image, likes"
-                    },
-                    locality:{
-                        type:"String",
-                        description:"Likes que tiene tu post",
-                        example:"6201064b0028de7866e2b2c4"
-                    },
-                    region:{
-                        type:"String",
-                        description:"Comentarios dentro del post, es un array de objetos",
-                        example:"userId, comment, image, likes"
-                    },
-                    country:{
-                        type:"String",
-                        description:"Comentarios dentro del post, es un array de objetos",
-                        example:"userId, comment, image, likes"
-                    },
-                    industry:{
-                        type:"String",
-                        description:"Comentarios dentro del post, es un array de objetos",
-                        example:"userId, comment, image, likes"
-                    },
-                    linkedin_url:{
-                        type:"String",
-                        description:"Comentarios dentro del post, es un array de objetos",
-                        example:"userId, comment, image, likes"
-                    }
-                }
+                        numberSize:{
+                            type:'object',
+                            description:"Number of companies by size",
+                            example:"1-10: 305,10001+: 3,1001-5000: 14,11-50: 160, ..."
+                        },
+                        numberIndustry:{
+                            type:'object',
+                            description:"Number of companies by type of industry",
+                            example:"null: 31,accounting: 11,airlines/aviation: 3,animation: 1, ..."
+                        },
+                        numberFounded:{
+                            type:"object",
+                            description:"Number of companies by year of creation",
+                            example:"1792: 1,1887: 1,1903: 1,1911: 1,1918: 1, ..."
+                        }
+                }           
             }
         }
     }
